ac250554a200dcc99007d36ff1ec68a8e0e539b0
mjimoh4 <mjimoh@montgomerycollege.edu>
2019-08-22 16:03:30 -0400

    Exercise on 4.01

---
+++ src/main/java/com/example/demo/SpringBoot401ExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot401ExerciseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot401ExerciseApplication.class, args);
+    }
+
+}
